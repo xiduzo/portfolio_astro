8f0f1fb7cd12981a77ca0585e72c9d8513bf0e9b
Sander <mail@sanderboer.nl>
2023-05-30 22:39:48 +0200

    use vercel image server

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind(), react()],
   output: "static",
   adapter: vercel({
+    imageService: true,
     imageConfig: {
       sizes: [320, 640, 1280],
     },

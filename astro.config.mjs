029e8385d04c59b4e72f14b2db218aebdd313f3a
Sander <mail@sanderboer.nl>
2023-05-30 15:07:48 +0200

    add layouts to git
    i think i pressed a wrong button

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,12 @@ import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sanderboer.nl",
+  site: "http://sanderboer.nl",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   output: "static",
-  adapter: vercel(),
+  adapter: vercel({
+    imageConfig: {
+      sizes: [320, 640, 1280],
+    },
+  }),
 });

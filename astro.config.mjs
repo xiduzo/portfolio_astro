c86b08f70a9b1e9890105450656e973c29371f48
Sander <mail@sanderboer.nl>
2023-05-30 14:53:38 +0200

    static vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sanderboer.nl",
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercel(),
 });

da360ad90470c8dbc4b38bf917aa6b5c9e70f856
Sander <mail@sanderboer.nl>
2024-02-19 18:51:40 +0100

    bump packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/static";
 import compress from "astro-compress";
+import * as transformers from 'shikiji-transformers';
 
+
+console.log(Object.values(transformers))
 const isProduction = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
@@ -15,10 +18,18 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
-      experimentalThemes: {
-        light: 'github-light',
-        dark: 'github-dark'
-      }
+      // https://docs.astro.build/en/guides/markdown-content/#shiki-configuration
+      // https://shikiji.netlify.app/packages/transformers
+      transformers: Object.values(transformers),
+      theme: 'github-dark',
+      // experimentalThemes: {
+      //   light: 'github-light',
+      //   dark: 'github-dark'
+      // },
+      wrap: true,
+      langs: [
+        
+      ]
     },
   },
   integrations: [
@@ -40,7 +51,14 @@ export default defineConfig({
   ],
   output: "static",
   outDir: "./.vercel/output/static",
-  experimental: {},
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  experimental: {
+    optimizeHoistedScript: true,
+    clientPrerender: true,
+  },
   adapter: vercel({
     analytics: isProduction,
     imageService: isProduction,

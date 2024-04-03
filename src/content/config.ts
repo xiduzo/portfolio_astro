75f3dfb4b776d3f5197a2e59b8c14e817b3d9d32
Sander <mail@sanderboer.nl>
2024-04-03 11:47:53 +0200

    add shareable image?

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,27 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const type = {
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		hidden: z.boolean().optional(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		headerColor: z.string(),
-		contrastColor: z.string().optional(),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string(),
-	}),
-}
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    hidden: z.boolean().optional(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    headerColor: z.string(),
+    contrastColor: z.string().optional(),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string(),
+    shareImage: z.string().optional(),
+  }),
+};
 
 const blog = defineCollection(type);
-const project = defineCollection(type)
+const project = defineCollection(type);
 
 export const collections = { blog, project };
